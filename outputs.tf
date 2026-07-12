750ffe45c6b4c87a5ab8c161fd4bd44bbc7f3360
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:11 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,25 @@
-output "batch_jobs" {
-  description = "All batch_job resources"
-  value       = azurerm_batch_job.batch_jobs
-}
 output "batch_jobs_batch_pool_id" {
-  description = "List of batch_pool_id values across all batch_jobs"
-  value       = [for k, v in azurerm_batch_job.batch_jobs : v.batch_pool_id]
+  description = "Map of batch_pool_id values across all batch_jobs, keyed the same as var.batch_jobs"
+  value       = { for k, v in azurerm_batch_job.batch_jobs : k => v.batch_pool_id }
 }
 output "batch_jobs_common_environment_properties" {
-  description = "List of common_environment_properties values across all batch_jobs"
-  value       = [for k, v in azurerm_batch_job.batch_jobs : v.common_environment_properties]
+  description = "Map of common_environment_properties values across all batch_jobs, keyed the same as var.batch_jobs"
+  value       = { for k, v in azurerm_batch_job.batch_jobs : k => v.common_environment_properties }
 }
 output "batch_jobs_display_name" {
-  description = "List of display_name values across all batch_jobs"
-  value       = [for k, v in azurerm_batch_job.batch_jobs : v.display_name]
+  description = "Map of display_name values across all batch_jobs, keyed the same as var.batch_jobs"
+  value       = { for k, v in azurerm_batch_job.batch_jobs : k => v.display_name }
 }
 output "batch_jobs_name" {
-  description = "List of name values across all batch_jobs"
-  value       = [for k, v in azurerm_batch_job.batch_jobs : v.name]
+  description = "Map of name values across all batch_jobs, keyed the same as var.batch_jobs"
+  value       = { for k, v in azurerm_batch_job.batch_jobs : k => v.name }
 }
 output "batch_jobs_priority" {
-  description = "List of priority values across all batch_jobs"
-  value       = [for k, v in azurerm_batch_job.batch_jobs : v.priority]
+  description = "Map of priority values across all batch_jobs, keyed the same as var.batch_jobs"
+  value       = { for k, v in azurerm_batch_job.batch_jobs : k => v.priority }
 }
 output "batch_jobs_task_retry_maximum" {
-  description = "List of task_retry_maximum values across all batch_jobs"
-  value       = [for k, v in azurerm_batch_job.batch_jobs : v.task_retry_maximum]
+  description = "Map of task_retry_maximum values across all batch_jobs, keyed the same as var.batch_jobs"
+  value       = { for k, v in azurerm_batch_job.batch_jobs : k => v.task_retry_maximum }
 }
 
